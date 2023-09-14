4864e0e7b96490977758042b6cef7e3000bfd0bd
Nurwahib Zakki Yahya <nurwahibzakkiy@gmail.com>
2023-09-14 19:33:55 +0700

    Joobsheet3

---
+++ ContohVariabel21.java
@@ -0,0 +1,19 @@
+public class ContohVariabel21 {   
+public static void main(String[] args) {
+   String  salahSatuHobiSayaAdalah= "Berenang";
+   boolean isPintar= true;
+   char jenisKelamin = 'L';
+   byte _umurSayaSekarang = 18;
+   double $ipk = 3.8, tinggi = 1.66;
+   System.out.println("Apa hobi kamu?" + salahSatuHobiSayaAdalah);
+   System.out.println("apakah pandai?" + isPintar);
+   System.out.println("Jenis kelamin:" + jenisKelamin );
+   System.out.println("Umurku saat ini:" + _umurSayaSekarang);
+   System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+}
+
+
+
+
+}
